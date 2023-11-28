5a1e2595261108004599363d5494f0f408f5a949
sensei <leesensei@leesensei.com>
2023-11-29 00:16:54 +0200

    updates

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 Implementing the very first neural style transfer algorithm from scratch
 
 Backbone: VGG19
+
+Example
+
+Content image - Liberty Statue in Riga
+![style_transfer](assets/liberty.jpg)
+Style image - Starry Night:
+![style_transfer](assets/starry_night.jpg) 
+
+Generated image:
+![style_transfer](assets/starry_liberty.jpg)
+
+Some observations:
+
+1. When initialize the target image to the content image, the target appears to go towards style image. The rate that it approaches the style image is highly dependent on the ratio between content loss and style loss.
+
+2. When the content image and the style image has similar size, the target image has better visual results.
+
--- style_transfer.py
+++ style_transfer.py
@@ -11,156 +11,172 @@ import torchvision.transforms.functional as TF
 import torchvision.transforms as T
 from PIL import Image
 import os
+from argparse import ArgumentParser
+from datetime import datetime
+from tqdm import tqdm
+parser = ArgumentParser()
+parser.add_argument("-epochs", type=int, default=2001)
+# weights=[0.001,1] worked
+parser.add_argument("-weights", type=float, nargs=2, default=[0.1, 1])
+parser.add_argument("-content_resize_size",
+                    type=int, nargs=2, default=[300, 300])
+parser.add_argument("-style_resize_size", type=int,
+                    nargs=2, default=[300, 300])
+parser.add_argument("-saving_frequency", type=int, default=10)
+
+args = parser.parse_args()
 torch.set_printoptions(precision=4)
-weights=VGG19_Weights.DEFAULT
-mean=[0.485, 0.456, 0.406]
-std=[0.229, 0.224, 0.225]
-transforms=T.Normalize(mean=mean,std=std)
-model=models.vgg19(weights=weights)
+weights = VGG19_Weights.DEFAULT
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+transforms = T.Normalize(mean=mean, std=std)
+model = models.vgg19(weights=weights)
 for p in model.parameters():
-  p.requires_grad=False
+    p.requires_grad = False
 
 
-hard_sigmoid=nn.Hardsigmoid()
-log_sigmoid=nn.LogSigmoid()
-hard_tanh=nn.Hardtanh()
-device="cuda" if torch.cuda.is_available() else "cpu"
-model.cuda()
+hard_sigmoid = nn.Hardsigmoid()
+log_sigmoid = nn.LogSigmoid()
+hard_tanh = nn.Hardtanh()
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
 model.eval()
 
 global sequential_outputs
-sequential_outputs=[]
+sequential_outputs = []
+
 
 def change_pooling_layer(model):
-  for i,f in enumerate(model.features):
-    if isinstance(f,nn.MaxPool2d):
-      model.features[i]=nn.AvgPool2d(f.kernel_size)
+    for i, f in enumerate(model.features):
+        if isinstance(f, nn.MaxPool2d):
+            model.features[i] = nn.AvgPool2d(f.kernel_size)
 
-def compose(x,op="none"):
-  if op=="tanh":
-    return (torch.tanh(x)+1)/2
-  elif op=="sigmoid":
-    return torch.sigmoid(x)
-  elif op=="hs":
-    return hard_sigmoid(x)
-  elif op=="ls":
-    return log_sigmoid(x)
-  elif op=="minmax":
-    x=(x+torch.min(x))/torch.max(x)
-    return x
-  elif op=="relu":
-    return torch.relu(x)
-  elif op=="shrink":
-    return torch.hardshrink(x)+0.5
-  elif op=="ht":
-    return hard_tanh(x)+1
-  elif op=="sin":
-     return torch.abs(torch.sin(x))
-  else:
-    return x
-def forward_hook(self,input,output):
+
+def compose(x, op="none"):
+    if op == "tanh":
+        return (torch.tanh(x)+1)/2
+    elif op == "sigmoid":
+        return torch.sigmoid(x)
+    elif op == "hs":
+        return hard_sigmoid(x)
+    elif op == "ls":
+        return log_sigmoid(x)
+    elif op == "minmax":
+        x = (x+torch.min(x))/torch.max(x)
+        return x
+    elif op == "relu":
+        return torch.relu(x)
+    elif op == "shrink":
+        return torch.hardshrink(x)+0.5
+    elif op == "ht":
+        return hard_tanh(x)+1
+    elif op == "sin":
+        return torch.abs(torch.sin(x))
+    else:
+        return x
+
+
+def forward_hook(self, input, output):
     sequential_outputs.append(output)
 
 
-def clear_output_list(self,input):
-  global sequential_outputs
-  sequential_outputs.clear()
+def clear_output_list(self, input):
+    global sequential_outputs
+    sequential_outputs.clear()
+
 
 def compute_gram_matrix(channel_outputs):
-  if channel_outputs.ndim!=4 and channel_outputs.ndim!=3:
-    raise RuntimeError("inputs must have either 3 or 4 dimensions")
-  if channel_outputs.ndim==4:
-    channel_outputs=channel_outputs.squeeze(0).flatten(start_dim=1)
-  gram=channel_outputs@(channel_outputs.permute(1,0))
-  return gram,channel_outputs.numel()
-
+    if channel_outputs.ndim != 4 and channel_outputs.ndim != 3:
+        raise RuntimeError("inputs must have either 3 or 4 dimensions")
+    if channel_outputs.ndim == 4:
+        channel_outputs = channel_outputs.squeeze(0).flatten(start_dim=1)
+    gram = channel_outputs@(channel_outputs.permute(1, 0))
+    return gram, channel_outputs.numel()
 
 
 model.features.register_forward_pre_hook(clear_output_list)
-for n,m in model.features.named_modules():
-  m.register_forward_hook(forward_hook)
+for n, m in model.features.named_modules():
+    # register forward hook to get the outputs of hidden layers
+    m.register_forward_hook(forward_hook)
+
+run_time = datetime.now().isoformat(timespec="minutes")
+content_image_path = "./assets/liberty.jpg"
+style_image_path = "./assets/starry_night.jpg"
+picname = content_image_path.split('/')[-1][:-4]
+result_root = f"./results/{picname}_{run_time}"
+if not os.access(result_root, os.F_OK):
+    os.makedirs(result_root)
+
+content_image = Image.open(content_image_path)
+content_image_size = [content_image.size[1], content_image.size[0]]
+style_image = Image.open(style_image_path)
+style_image_size = [style_image.size[1], style_image.size[0]]
 
 
-content_image_path="/content/style_transfer_pics/photo.jpg"
-style_image_path="/content/style_transfer_pics/style.jpg"
-picname=content_image_path.split('/')[-1][:-4]
-result_root=f"/content/drive/MyDrive/style_results/{picname}"
-if not os.access(result_root,os.F_OK):
-  os.makedirs(result_root)
-
-content_image=Image.open(content_image_path)
-content_image_size=[content_image.size[1],content_image.size[0]]
-style_image=Image.open(style_image_path)
-style_image_size=[style_image.size[1],style_image.size[0]]
-
-
-
-content_resize_size=[300,300]
-style_resize_size=[300,300]
-
-content_image_tensor=TF.to_tensor(TF.resize(content_image,content_resize_size)).unsqueeze(0).to(device)
-style_image_tensor=TF.to_tensor(TF.resize(style_image,style_resize_size)).unsqueeze(0).to(device)
-#change_pooling_layer(model)
+content_image_tensor = TF.to_tensor(
+    TF.resize(content_image, args.content_resize_size)).unsqueeze(0).to(device)
+style_image_tensor = TF.to_tensor(
+    TF.resize(style_image, args.style_resize_size)).unsqueeze(0).to(device)
+# change_pooling_layer(model)
 with torch.no_grad():
-  model(compose(content_image_tensor))
-  content_outputs=copy(sequential_outputs)
-  model(compose(style_image_tensor))
-  style_outputs=copy(sequential_outputs)
+    model(compose(content_image_tensor))
+    content_outputs = copy(sequential_outputs)
+    model(compose(style_image_tensor))
+    style_outputs = copy(sequential_outputs)
 
-style_grams=[]
+style_grams = []
 for out in style_outputs:
-  style_gram,_=compute_gram_matrix(out)
-  style_grams.append(style_gram)
+    style_gram, _ = compute_gram_matrix(out)
+    style_grams.append(style_gram)
 
-#target_image=torch.randn((1,3,*content_resize_size),requires_grad=True,device=device)
-target_image=content_image_tensor.clone()
-target_image.requires_grad=True
-epoch=4001
+target_image = content_image_tensor.clone()
+target_image.requires_grad = True
 
-optimizer=torch.optim.LBFGS([target_image],lr=0.1)
-style_loss_fn=nn.MSELoss()
-content_loss_fn=nn.MSELoss()
-all_relus=torch.tensor([1,3,6,8,11,13,15,17,20,22,24,26,27,29,31,33,35])
-selected_style_layers=all_relus[[0,2,4,8,13]]
+optimizer = torch.optim.LBFGS([target_image], lr=0.1)
+style_loss_fn = nn.MSELoss()
+content_loss_fn = nn.MSELoss()
+all_relus = torch.tensor(
+    [1, 3, 6, 8, 11, 13, 15, 17, 20, 22, 24, 26, 27, 29, 31, 33, 35])
+selected_style_layers = all_relus[[0, 2, 4, 8, 13]]
 
-selected_content_layers=all_relus[[9]]
-print(f"style activation layers: {selected_style_layers};content activation layers: {selected_content_layers}")
+selected_content_layers = all_relus[[9]]
+for k, v in args.__dict__.items():
+    print(f"{k} : {v}")
+print(
+    f"style activation layers: {selected_style_layers};content activation layers: {selected_content_layers}")
+for e in tqdm(range(args.epochs)):
+    def closure():
+        optimizer.zero_grad()
+        with torch.no_grad():
+            target_image.clamp_(0, 1)
+        model(compose(target_image))
+        # compute style loss
+        target_sequential_outputs = copy(sequential_outputs)
+        style_loss = 0
+        for layer in selected_style_layers:
+            target_gram, numel = compute_gram_matrix(
+                target_sequential_outputs[layer])
+            style_loss += style_loss_fn(target_gram, style_grams[layer])/numel
 
-#weights=[0.001,1] worked
-weights=[0.1,1]
+        # compute content loss
+        content_loss = 0
+        for l in selected_content_layers:
+            content_loss += content_loss_fn(
+                target_sequential_outputs[l], content_outputs[l])
+        loss = args.weights[0]*content_loss+args.weights[1] * \
+            style_loss/len(selected_style_layers)
 
-for e in range(epoch):
-  def closure():
-    optimizer.zero_grad()
-    with torch.no_grad():
-      target_image.clamp_(0,1)
-    model(compose(target_image))
-    #compute style loss
-    target_sequential_outputs=copy(sequential_outputs)
-    style_loss=0
-    for layer in selected_style_layers:
-      target_gram,numel=compute_gram_matrix(target_sequential_outputs[layer])
-      style_loss+=style_loss_fn(target_gram,style_grams[layer])/numel
+        loss.backward(retain_graph=True)
 
-    #compute content loss
-    content_loss=0
-    for l in selected_content_layers:
-      content_loss+=content_loss_fn(target_sequential_outputs[l],content_outputs[l])
-    loss=weights[0]*content_loss+weights[1]*style_loss/len(selected_style_layers)
-
-    loss.backward(retain_graph=True)
-    #print(f"epoch: {e};loss: {loss.detach().round(decimals=4)},unweighted content loss: {content_loss.detach().round(decimals=4)},unweighted style loss: {style_loss.detach().round(decimals=4)}")
-
-    return loss
-  optimizer.step(closure)
-  if e%100==0:
-    #print(f"epoch: {e};loss: {loss.detach().round(decimals=4)},unweighted content loss: {content_loss.detach().round(decimals=4)},unweighted style loss: {style_loss.detach().round(decimals=4)}")
-    fig,axes=plt.subplots(1,3,constrained_layout=True)
-    for a in axes:
-      a.axis("off")
-    generated_image=TF.to_pil_image(compose(target_image.detach()).squeeze(0).cpu())
-    axes[0].imshow(content_image.resize(style_resize_size))
-    axes[1].imshow(style_image.resize(style_resize_size))
-    axes[2].imshow(generated_image)
-    plt.show()
-    generated_image.save(f"{result_root}/{picname}_{e}.jpg")
+        return loss
+    optimizer.step(closure)
+    if e % args.saving_frequency == 0:
+        fig, axes = plt.subplots(1, 3, constrained_layout=True)
+        for a in axes:
+            a.axis("off")
+        generated_image = TF.to_pil_image(
+            compose(target_image.detach()).squeeze(0).cpu())
+        axes[0].imshow(content_image.resize(args.style_resize_size))
+        axes[1].imshow(style_image.resize(args.style_resize_size))
+        axes[2].imshow(generated_image)
+        generated_image.save(f"{result_root}/{picname}_{e}.jpg")
